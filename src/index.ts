c2949d720579fb05dc5acf902e812047af82a51a
goto-jesus <vadim.tkachenko.1997@gmail.com>
2023-07-27 13:04:13 +0300

    examples

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
 'use strict';
 import { XmlFormater } from './formates/xml';
 
+//* Example 1
+
 const xml = new XmlFormater();
 xml.read('output.xml');
 xml.add('BMW', 77_000);
+xml.remove(0);
+// xml.convertTo(new BinaryFormater);
+// xml.convertTo(new JsonFormater);
 xml.show();
 // xml.write('added_BMW.xml');
 
-//  npm start
+// ------------------------------------------------------------------------------- */
+
+/* Example 2
+
+const bin = new BinaryFormater();
+bin.add('Lamborghini', 507_000);
+bin.add('Ferrari', 361_500);
+bin.write('sport cars.bin');
+
+// bin.convertFile('sport cars.bin', new JsonFormater);
+
+// ------------------------------------------------------------------------------- */
+
